d3e66a955adbe0415f4203dd843a714920ae3b6d
Emmanuel DEMEY <demey.emmanuel@gmail.com>
2017-08-31 13:59:43 +0200

    Init Express and add rule for base branch for a new PR

---
+++ config.js
@@ -0,0 +1,7 @@
+module.exports = {
+    baseUrl: 'https://api.github.com',
+    userAgent: 'Love Bot',
+    GITHUB_TOKEN: process.env.GITHUB_TOKEN,
+
+    BASE_PULLREQUEST_BRANCH: 'dev',
+}
---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const utils = require('./utils');
+const config = require('./config');
+
+app.use(bodyParser.json());
+
+app.post('/pullrequest', function (req, res) {
+    const owner = req.body.repository.owner.login
+    const repo = req.body.repository.name;
+    const sha = req.body.pull_request.head.sha;
+    const context = 'LoveBot/PR/BranchName'
+    let state = 'success';
+    let description = 'You have selected the right branch';
+    
+    if(req.body.pull_request.base.ref !== config.BASE_PULLREQUEST_BRANCH){
+        state = 'error';
+        description = `You should use the ${config.BASE_PULLREQUEST_BRANCH} as the base branch`;
+    }
+
+    utils.updateStatus(owner, repo, sha, state, description, context);
+    res.send('Hello World!');
+});
+
+
+app.listen(8080, function () {
+  console.log('Example app listening on port 3000!');
+});
